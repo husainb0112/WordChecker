13bc3f183df0c07373c10d56e4f5c92c98cb852b
husainb0112 <husainb@nycstudents.net>
2024-10-09 13:33:08 +0000

    v1

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    
+    public static void main(String args[]) {
+        
+        WordChecker w = new WordChecker();
+        wordList = new ArrayList<String>();
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("an");
+        words.add("band");
+        words.add("band");
+        words.add("abandon"); 
+
+    }
+
+}
---
+++ WordChecker.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class WordChecker {
+
+    private ArrayList<String> wordList;
+    
+    public WordChecker(ArrayList<String> list) {
+
+        wordList = list;
+    }
+
+    
+
+    public boolean isWordChain() {}
+
+    public ArrayList<String> createList(String target) {}
+}
