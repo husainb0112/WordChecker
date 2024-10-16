232f8b1865bbfd960f4bf06aeb4c4a5998f75a80
husainb0112 <husainb@nycstudents.net>
2024-10-16 13:32:50 +0000

    v2

--- Main.java
+++ Main.java
@@ -1,15 +1,21 @@
+import java.util.ArrayList;
 public class Main {
     
     public static void main(String args[]) {
-        
-        WordChecker w = new WordChecker();
-        wordList = new ArrayList<String>();
-        ArrayList<String> words = new ArrayList<String>();
+
+        ArrayList<String> words = new ArrayList<>();
         words.add("an");
         words.add("band");
         words.add("band");
-        words.add("abandon"); 
+        words.add("abandon");
+        System.out.println(words);
+        
+        WordChecker x = new WordChecker(words);
+        System.out.println(x.isWordChain());
+
+
+
+        
 
     }
-
 }
--- WordChecker.java
+++ WordChecker.java
@@ -1,16 +1,28 @@
 import java.util.ArrayList;
 public class WordChecker {
-
-    private ArrayList<String> wordList;
     
-    public WordChecker(ArrayList<String> list) {
+    private ArrayList<String> wordList;
 
+    public WordChecker() {
+        wordList = new ArrayList<>();
+    }
+
+    public WordChecker(ArrayList<String> list) {
         wordList = list;
+    }
+    
+    public boolean isWordChain() {
+        boolean wordChain = true;
+        for(int i = 1; i < wordList.size(); i++) {
+            String after = wordList.get(i);
+            String before = wordList.get(i - 1);
+            if(after.indexOf(before) < 0) return false;
+
+        }
+        return wordChain;
     }
 
     
-
-    public boolean isWordChain() {}
-
-    public ArrayList<String> createList(String target) {}
+    /*public ArrayList<String> createList(String target) {
+        }*/
 }
